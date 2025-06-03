e06051ba9ff9c454f5da514e8e2d2503852337e2
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-06-04 00:43:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 7/pattern-7.java
@@ -0,0 +1,18 @@
+class Solution {
+  void printTriangle(int n) {
+     for (int i = 0; i < n; i++) {
+            // Print spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+  }
+}
