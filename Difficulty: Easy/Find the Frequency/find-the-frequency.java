e33a92c8c7d8da98d323d8add8e0042f2cd6e468
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-06-17 03:29:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the Frequency/find-the-frequency.java
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.java
@@ -1,11 +1,27 @@
-import java.util.HashMap;
-
 class Solution {
     int findFrequency(int arr[], int x) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int num : arr) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        // code here
+        int n = arr.length;
+        HashMap <Integer,Integer> map = new HashMap<>();
+        // traversing using for loop ;
+        for ( int i = 0 ; i < n ; i ++){
+            int num = arr[i];
+            
+        // now we check if we have the element present or not in hash map 
+        if (map.containsKey(num)){
+            int count = map.get(num);
+            map.put(num, count +1);
         }
-        return freq.getOrDefault(x, 0);
+        else {
+            map.put(num,1);
+        }
+        }
+        if (map.containsKey(x)){
+            return map.get(x);
+        }
+        else {
+            return 0 ; 
+        }
     }
+    
 }
