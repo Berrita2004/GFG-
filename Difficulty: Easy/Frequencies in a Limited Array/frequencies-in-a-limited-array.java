061ef8ca85213e12459fd84cc7a03bf7f89e5449
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-06-16 23:29:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.java
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.java
@@ -6,6 +6,9 @@ class Solution {
         int [] hash= new int [n+1];
         for ( int i = 0; i < n ; i ++){
             if(arr[i]>=1&& arr[i]<=n){
+                //ye issilye kyuli hash value jo store karta hai woh 0-n kar raha hai 
+                // if  i take n = 13 , hash ke pass 13 ki position hogi but hash ke pass 14 ki nahi hogi na 
+        //kyuki 14 toh hash ke bhar chala gaya , hash har ek element ke liye ek value hi rakhta hai 
             hash[arr[i]]+= 1;
         }
         }
