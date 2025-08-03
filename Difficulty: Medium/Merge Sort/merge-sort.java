90e654333d80106b69ba246503d185a734aeb39d
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-08-03 23:02:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,45 +1,50 @@
 class Solution {
-private static void merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-        int left = low;      // starting index of left half of arr
-        int right = mid + 1;   // starting index of right half of arr
-
-        //storing elements in the temporary array in a sorted manner//
-
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.add(arr[left]);
-                left++;
-            } else {
-                temp.add(arr[right]);
-                right++;
-            }
-        }
-
-        // if elements on the left half are still left //
-
-        while (left <= mid) {
-            temp.add(arr[left]);
-            left++;
-        }
-
-        //  if elements on the right half are still left //
-        while (right <= high) {
-            temp.add(arr[right]);
-            right++;
-        }
-
-        // transfering all elements from temporary to arr //
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp.get(i - low);
-        }
-    }
-    void mergeSort(int arr[], int low, int high) {
+   void merge(int arr[], int l , int mid , int r ){
+       int n = arr.length;
+       int [] temp = new int [r-l+1] ;
+       int left = l ; 
+       int right = mid +1;
+       int idx= 0 ; 
+       
+       while(left <= mid && right <= r){
+           if ( arr[left]<= arr[right]) {
+               temp[idx] = arr[left];
+               left ++;
+               idx++;
+           }
+       else {
+            temp[idx] = arr[right];
+           right++;
+           idx++;
+       }
+           
+       }
+       
+       while (left <= mid ){
+            temp[idx] = arr[left];
+           left++;
+           idx++;
+           
+       }
+       while (right <= r){
+           temp[idx] = arr[right];
+           right++;
+           idx++;
+           
+       }
+       for( int i = 0 ; i < temp.length; i ++){
+           arr[l+i]= temp[i];
+       }
+     
+       
+       
+   }
+    void mergeSort(int arr[], int l, int r) {
         // code here
-        if (low >= high) return;
-        int mid = (low + high) / 2 ;
-        mergeSort(arr, low, mid);  // left half
-        mergeSort(arr, mid + 1, high); // right half
-        merge(arr, low, mid, high);  // merging sorted halves
+     if ( l >= r) return ; 
+     int mid = (l+r )/2 ;
+     mergeSort(arr,  l,  mid );
+     mergeSort(arr, mid +1 ,  r );
+     merge( arr,  l ,  mid,  r);
     }
 }
