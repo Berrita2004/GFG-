6c4f5aa368d05a1ef73e9a711361217fcc9dd076
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-08-16 20:15:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,27 +1,37 @@
 class Solution {
     public int aggressiveCows(int[] stalls, int k) {
         // code here
-        int n = stalls.length;
         Arrays.sort(stalls);
-        int range = stalls[n-1]- stalls[0];
-        for ( int i = 1 ; i <= range ; i++){
-            if (canPlace(stalls, i, k) == false)  {   return i-1;}
-        }
-        return range ;
-    }
-    public static boolean canPlace(int[] stalls, int dist, int cows){
         int n = stalls.length;
-        int last = stalls[0];
-        int cntcow = 1 ;
+        int low = 1 ;
+        int high = stalls[n-1]- stalls[];
+        while ( low <= high){
+            int mid = (low + high)/2;
+            if (canPlace(stalls, mid , k)==true){
+                low = mid + 1 ;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return high ;
         
-        for ( int i = 1; i < n ; i++){
-            if ((stalls[i]- last)>= dist) {
-                cntcow++; 
-            last = stalls[i];}
-            if (cntcow >= cows) return true;
+    }
+    public static boolean canPlace (int[] stalls , int dist , int cows){
+        int last = stalls[0];
+        int n = stalls.length;
+        int cnt = 1 ; 
+        for (int i = 1 ; i < n ; i ++){
+            if ((stalls[i]-last )>= dist) {
+                cnt++;
+                
+                last = stalls[i];
+                 if (cnt>= cows) return true ;
+                
+            }
             
         }
-        return false;
+       
+        return false ;
     }
-    
 }
