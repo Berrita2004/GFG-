64834806d1ed7206b847946995d86c1ebe680ae5
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-23 15:56:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Previous Greater Element/previous-greater-element.java
+++ Difficulty: Medium/Previous Greater Element/previous-greater-element.java
@@ -2,15 +2,17 @@ class Solution {
     static ArrayList<Integer> preGreaterEle(int[] arr) {
         // code here
             int n = arr.length;
-            ArrayList<Integer> ans = new ArrayList<>();
+            ArrayList<Integer> ans = new ArrayList<>(n);
             Stack <Integer> st = new Stack<>();
+            for (int i = 0; i < n; i++) ans.add(0);
             for ( int i = 0 ; i < n ; i++){
                 while (!st.isEmpty() && arr[st.peek()] <=arr[i]){
                     st.pop();
                 }
-                ans.add( st.isEmpty() ? -1 : arr[st.peek()]);
+                ans.set(i, st.isEmpty() ? -1 : arr[st.peek()]);
                 st.push(i);
             }
+            
             return ans ;
     }
 }
